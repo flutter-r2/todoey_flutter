e2d73dc2de0d60226e996f7c8ef05fa019f0d19f
Artur <arturbr@gmail.com>
2022-05-09 15:48:10 -0300

    Lifting checkbox state

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,13 +1,29 @@
 import 'package:flutter/material.dart';
 
 class TaskTile extends StatelessWidget {
+  final bool isChecked;
+  final String name;
+  final Function(bool?) onCheckboxChange;
+
+  // ignore: use_key_in_widget_constructors
+  const TaskTile(
+      {required this.name,
+      required this.isChecked,
+      required this.onCheckboxChange});
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('task 1'),
+      title: Text(
+        name,
+        style: TextStyle(
+          decoration: isChecked ? TextDecoration.lineThrough : null,
+        ),
+      ),
       trailing: Checkbox(
-        value: false,
-        onChanged: null,
+        activeColor: Colors.lightBlueAccent,
+        value: isChecked,
+        onChanged: onCheckboxChange,
       ),
     );
   }
--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -1,15 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:todoey_flutter/widgets/task_tile.dart';
 
-class TasksList extends StatelessWidget {
+import '../models/task.dart';
+
+class TasksList extends StatefulWidget {
+  @override
+  State<TasksList> createState() => _TasksListState();
+}
+
+class _TasksListState extends State<TasksList> {
+  List<Task> _tasks = [
+    Task(name: 'buy milk'),
+    Task(name: 'buy cookies'),
+    Task(name: 'buy bread'),
+    Task(name: 'buy eggs'),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        TaskTile(),
-        TaskTile(),
-        TaskTile(),
-      ],
+    return ListView.builder(
+      itemBuilder: (context, index) => TaskTile(
+        name: _tasks[index].name,
+        isChecked: _tasks[index].isDone,
+        onCheckboxChange: (bool? value) {
+          setState(() => _tasks[index].toggleDone());
+        },
+      ),
+      itemCount: _tasks.length,
     );
   }
 }
